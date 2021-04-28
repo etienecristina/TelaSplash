7e382dcd786bc227cf84fef570fb1613dd228fc3
Etiene Cristina <etiene.scristina@gmail.com>
2021-04-28 16:06:31 -0300

    Primeiro commit do projeto

---
+++ app/src/main/java/primeiro/cliente/cursoandroid/TelaSplashActivity.java
@@ -0,0 +1,25 @@
+package primeiro.cliente.cursoandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class TelaSplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_splash);
+
+            new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+               startActivity(new Intent(TelaSplashActivity.this, MainActivity.class));
+
+            }
+        },5000);
+
+    }
+}
